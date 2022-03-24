02acd3706642d234a679a8047c820abcb6358408
Mia Jarnot <ejarnot42@outlook.com>
2022-03-24 11:35:55 -0700

    Filled in the exercises with For loops

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for(let number = 1; number <= 100; number+=2) {
+    if(number){
+        console.log(`${number} and counting...`)
+    }else{
+        console.log(`do not print`)
+    }
+} 
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let newNumber = 1; newNumber<=100; newNumber++){
+    if(newNumber % 3 ==0 && newNumber % 5 == 0){
+        console.log("FIZZBUZZ")
+    }else if(newNumber % 5==0){
+            console.log("BUZZ")
+    }else if(newNumber % 3 == 0){
+        console.log("FIZZ")
+    }else{
+        console.log(newNumber)
+    }
+}
